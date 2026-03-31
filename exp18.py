85e93f57415896ec2576ff19e35fafafb2597ea4
mohammedali200907 <mohammedali@eng.rizvi.edu.in>
2026-03-31 20:18:09 +0530

    Add files via upload

---
+++ exp18.py
@@ -0,0 +1,11 @@
+print("Mohammed Ali, 251A007")
+
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([[1, 2], [3, 4]])
+
+print("1D Array:", arr1)
+print("2D Array:", arr2)
+
+print("Reshape:", arr1.reshape(3,1))
